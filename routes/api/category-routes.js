24334e1cabaa117bfd683a38e953009c59a9af3a
JBO <boldsja@gmail.com>
2022-04-23 15:41:21 -0500

    start up working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,10 +3,29 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+// router.get('/', (req, res) => {
+//   // find all categories
+//   // be sure to include its associated Products
+//   Category.findAll({
+//     include: [Product]
+//   })
+//     .then((categories) => res.json(categories))
+//     .catch((err) => res.status(500).json(err))
+// });
+
+router.get('/', async (req, res) => {
+
+  try {
+    const categories = await Category.findAll({
+      include: [Product]
+    })
+
+    res.status(200).json(categories)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
